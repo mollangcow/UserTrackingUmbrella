826591ec1d80d7b6a1f0f16b6fdb08e922fd478a
SangwooKim(Moca) <mollangcow@gmail.com>
2025-11-05 13:53:08 +0900

    first commit

---
+++ Umbrella/UmbrellaApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmbrellaApp.swift
+//  Umbrella
+//
+//  Created by mollangcow on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct UmbrellaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
